0d38933752984fad9c86c36b8ee45b05345b739c
Peter Ahlgren <ahlgren1234@gmail.com>
2024-03-21 10:08:03 +0100

    Updated tech stack info in footer.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Socials from "./Socials"
 
 const Footer = () => {
@@ -10,6 +11,19 @@ const Footer = () => {
           { /* copyright */}
           <div className="text-muted-foreground">
             Copyright &copy; Peter Ahlgren. All rights reserved.
+          </div>
+          { /* Tech Stack */ }
+          <div className="flex mt-4 gap-x-4 text-muted-foreground">
+            This webesite is created with:
+            <Image src="/footer/nextjs2.png" height={20} width={22} alt="Next JS" />
+            <Image src="/footer/js.png" height={20} width={22} alt="Javascript" />
+            <Image src="/footer/git.png" height={20} width={22} alt="Git" />
+            <Image src="/footer/figma.png" height={20} width={22} alt="Figma" />
+            <Image src="/footer/vscode.png" height={20} width={22} alt="VS Code" />
+          </div>
+          <div className="flex mt-4 gap-x-4 text-muted-foreground">
+            <a href="https://github.com/ahlgren1234/portfolio2021" target="_blank">You can study the code at Github</a>
+            <Image src="/footer/github.png" height={20} width={22} alt="GitHub" />
           </div>
         </div>
       </div>
