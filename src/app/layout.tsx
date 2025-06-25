ac283b078b7182297b4142dc2e466f4981b924ce
Peter Ahlgren <ahlgren1234@gmail.com>
2025-06-25 11:44:33 +0200

    fix error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <!-- Hotjar Tracking Code for https://peterahlgren.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:6445406,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+      </head>
       <body className={`${inter.className} antialiased`}>
         <Header />
         <ClientLayout caveatFont={caveat.style.fontFamily}>{children}</ClientLayout>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,6 @@ import Projects from '@/components/Projects';
 import Contact from '@/components/Contact';
 import Certifications from '@/components/Certifications';
 import Game from '@/components/Game';
-import Hotjar from '@hotjar/browser';
-
-const siteId = 6445406;
-const hotjarVersion = 6;
-
-Hotjar.init(siteId, hotjarVersion);
 
 export default function Home() {
   return (
