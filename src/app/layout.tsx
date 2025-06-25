3c661dc7abd6f211264837ef2bd3fb94199ce04c
Peter Ahlgren <ahlgren1234@gmail.com>
2025-06-25 11:35:21 +0200

    Add Hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,20 @@ import "./globals.css";
 import ClientLayout from "@/components/ClientLayout";
 import { Analytics } from "@vercel/analytics/react"
 import Header from "@/components/Header";
+import Hotjar from "@hotjar/browser";
 
 const inter = Inter({ subsets: ["latin"] });
 const caveat = Caveat({ subsets: ["latin"] });
+
+const siteId = 6445406;
+const hotjarVersion = 6;
 
 export const metadata: Metadata = {
   title: "Peter Ahlgren | Software Engineer",
   description: "Software engineer and web developer portfolio of Peter Ahlgren",
 };
+
+Hotjar.init(siteId, hotjarVersion);
 
 export default function RootLayout({
   children,
